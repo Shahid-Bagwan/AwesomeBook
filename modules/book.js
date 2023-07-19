4ef31ddb3278102dbb9475a67aaef867e05efd02
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-07-19 05:47:07 +0530

    Made changes suggested by the code reviewer

--- modules/book.js
+++ modules/book.js
@@ -13,24 +13,32 @@ export class SingleBook {
   add() {
     const containerForSingleBook = document.createElement('div');
     containerForSingleBook.classList.add('containerBooks');
-    containerForSingleBook.innerHTML = `<div class=bookDetails><p id='bookName'>${this.bookName}</p> <p id='by'>by</p> <p id='bookAuthor'>${this.bookAuthor}</p></div><button class="remove">remove</button>`;
+    containerForSingleBook.innerHTML = `<div class=bookDetails><p id='bookName'>${this.bookName}</p> <p id='by'>by</p> <p id='bookAuthor'>${this.bookAuthor}</p></div><button class="remove" data-index="${allBooks.length}">remove</button>`;
     containerForAllBooks.appendChild(containerForSingleBook);
+
     const singleBookObject = new SingleBook(this.bookName, this.bookAuthor);
     allBooks.push(singleBookObject);
     const ab = JSON.stringify(allBooks);
     localStorage.setItem('allBooks', ab);
+
     bookTextField.value = '';
     authorTextField.value = '';
   }
-  // deal with it
 
   static handleRemoveButtonClick() {
-    console.log('handleRemoveButtonClick');
     const removeButtons = document.getElementsByClassName('remove');
     for (let i = 0; i < removeButtons.length; i += 1) {
-      removeButtons[i].addEventListener('click', () => {
-        allBooks.splice(i, 1);
-        localStorage.setItem('allBooks', JSON.stringify(allBooks));
+      removeButtons[i].addEventListener('click', (event) => {
+        const button = event.target;
+        const bookIndex = button.dataset.index;
+
+        if (bookIndex !== undefined) {
+          allBooks.splice(bookIndex, 1);
+          localStorage.setItem('allBooks', JSON.stringify(allBooks));
+          // Optionally, you may also remove the book element from the DOM
+          // if it is rendered dynamically.
+          button.parentElement.remove();
+        }
       });
     }
   }
